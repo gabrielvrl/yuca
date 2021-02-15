995b083810d933dcdba6f6d0cbc8a76c17117c46
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-02-15 14:56:37 -0300

    Adding and changing button color

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import { withRouter } from 'react-router';
+import React, { useEffect } from 'react';
+import { withRouter, useLocation } from 'react-router';
 
 import './Menu.css';
 
 function Menu({ history }){
+    let location = useLocation()
+    useEffect(()=> {
+        async function changeButtonColor(){
+            if ('/myaccount' === location.pathname){
+                let account = document.getElementById("account")
+                account.style.color = "#13C081"
+                let yuca = document.getElementById("yuca")
+                yuca.style.color = "#000000"
+            } else {
+                let yuca = document.getElementById("yuca")
+                yuca.style.color = "#13C081"
+                let account = document.getElementById("account")
+                account.style.color = "#000000"
+            }
+        }
+        changeButtonColor()
+    },[location])
 
     function handleYuca(e){
         e.preventDefault()
@@ -18,8 +35,8 @@ function Menu({ history }){
     return(
         <div className="containerMenu">
             <div className="options">
-                <button onClick={handleYuca} >MEU YUCA</button>
-                <button onClick={handleConta} style={{color: "black"}} >MINHA CONTA</button>
+                <button id="yuca" onClick={handleYuca}>MEU YUCA</button>
+                <button id="account" onClick={handleConta}>MINHA CONTA</button>
             </div>
         </div>
     );
