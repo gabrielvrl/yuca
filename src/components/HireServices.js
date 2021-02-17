75a1f50fc922bee4bd38319108962850f54a2abb
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-02-16 23:33:22 -0300

    Fixing Checkbox

--- src/components/HireServices.js
+++ src/components/HireServices.js
@@ -10,7 +10,7 @@ import api from '../services/api'
 function HireServices({ history }){
     const [userServices, setUserServices] = useState([])
     useEffect(() => {
-        async function loadUser(){
+        async function loadUser() {
             const response = await api.get('/me', {})
             setUserServices(response.data.services)
         }
@@ -18,16 +18,18 @@ function HireServices({ history }){
     },[])
 
 
+    const [totalPrice, setTotalPrice] = useState(0)
+
+
     const [servicesOptions, setServicesOptions] = useState([])
     useEffect(() => {
     async function loadServices(){
         const promise = await api.get('/services', {})
-        setServicesOptions(promise.data)
+        const serializedUserServices = promise.data.map(service => ({ ...service, checked: false }))
+        setServicesOptions(serializedUserServices)
     }
     loadServices()
     },[])
-
-
 
 
     function handleCancel(e){
@@ -42,43 +44,59 @@ function HireServices({ history }){
 
     const GreenCheckbox = withStyles({
         root: {
-          color: "#AAAAAA",
-          padding: 0,
-          '&$checked': {
-            color: "#13c081",
-          },
-        },
-        checked: {},
-      })((props) => <Checkbox color="default" {...props} />);
+                color: "#AAAAAA",
+                padding: 0,
+                '&$checked': {
+                    color: "#13c081",
+                },
+            },
+            checked: Boolean,
+    })((props) => <Checkbox color="default" {...props} />);
 
-    const optionsCode = servicesOptions.map((service)=> (
-        <div className="servicesOptions" key={service.id}>
-            <GreenCheckbox
-                style={{ transform: "scale(1.2)"}}
-                name={service.name} 
-                value={Number(service.price)}
-            />
-            <label id="nameLabel">{service.name}</label>
-            <div className="priceLabel">
-                <label>R${service.price}</label>
+
+    function checkChecked({ id }) {
+        const serializedServices = servicesOptions.map((service) =>
+            service.id === id
+            ? ({ ...service, checked: !service.checked })
+            : service
+        )
+        setServicesOptions(serializedServices)
+    }
+
+    function optionsCode(){
+        return(
+            servicesOptions.map((service)=> (
+            <div className="servicesOptions" key={service.id}>
+                <GreenCheckbox
+                    style={{ transform: "scale(1.2)"}}
+                    name={service.name} 
+                    value={Number(service.price)}
+                    checked={service.checked}
+                    onChange={() => checkChecked(service)}
+                />
+                <label id="nameLabel">{service.name}</label>
+                <div className="priceLabel">
+                    <label>R${service.price}</label>
+                </div>
             </div>
-        </div>
-    ))
+        ))
+        )
+    }
 
     return(
         <div className="containerHire">
             <hr />
             <h1>Serviços Contratados</h1>
             <div className="services">
-                {optionsCode}
+                {optionsCode()}
                 <hr id="TotalHR"></hr>
-                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                <div className="totalSelected">
                     <h6 id="totalLabel">Total selecionado</h6>
-                    <h6 id="totalNumberLabel">R$110</h6>
+                    <h6 id="totalNumberLabel">R${totalPrice}</h6>
                 </div>
                 <div className="finalButtons">
-                    <button style={{ marginRight: "3%" }}  onClick={handleCancel}>CANCELAR</button>
-                    <button style={{ backgroundColor: "#13C081", color: 'white' }} onClick={handleSave} type="submit">SALVAR</button>
+                    <button id="cancelButton" onClick={handleCancel}>CANCELAR</button>
+                    <button id="saveButton" onClick={handleSave} type="submit">SALVAR</button>
                 </div>
             </div>
         </div>
