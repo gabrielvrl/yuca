ce45e89273ad1a1f920a85bcbb4e33244b6bd688
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-02-24 11:39:16 -0300

    Minor change

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Header from './components/Header.js';
+import Header from './components/Header';
 import Menu from './components/Menu';
 import Home from './components/Home';
 import HireServices from './components/HireServices'
