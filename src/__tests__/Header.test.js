03e08e4ff06ba50e0f9813715a9d69abbcf72706
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-02-25 10:55:12 -0300

    After npm run eject, and i was able to fix the bug from the tests

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,34 +1,12 @@
 import { render } from '@testing-library/react'
 import React from 'react'
+import { Route, Router, withRouter } from 'react-router-dom'
 import Header from '../components/Header'
 
 it("should render Header component", () => {
     render(
-        <Header />,
-        document.getElementsByClassName('headerDiv')
+      <Router>
+        <Header />
+      </Router>
     )
 })
-/* 
-describe("Button component", () => {
-    test("Matches the snapshot", () => {
-      const button = create(<Header />);
-      expect(button.toJSON()).toMatchSnapshot();
-    });
-  }); */
-/* 
-  ,
-  "jest": {
-    "moduleNameMapper": {
-      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-      "\\.(css|less|scss|sass)$": "identity-obj-proxy"
-    },
-    "transform": {
-      "^.+\\.[t|j]sx?$": "babel-jest"
-    }
-  } */
-
-/*   ,
-    "babel-jest": "^26.6.3",
-    "jest": "^26.6.3",
-    "jest-dom": "^4.0.0",
-    "react-test-render": "^1.1.2" */
